2c6c76418f443d9d5bfe0f56173b1403d3f4dc9d
Tihomir Marinov <tihomir.j.marinov@gmail.com>
2026-01-02 22:57:15 +0200

    Update next version

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-
 };
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
